513628d2618fbd5bbbce3cfe91acce6d0efb8ba0
MikolajP13 <mikolajp1313@gmail.com>
2024-11-13 18:44:49 +0100

    Implemented basic tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,54 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let element: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+      providers: [provideRouter([])],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        element = fixture.debugElement;
+      });
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the component instance', () => {
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'SimpleFavLists' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('SimpleFavLists');
+    expect(component.title).toEqual('SimpleFavLists');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SimpleFavLists');
+  it(`should have a container with the class 'app-container'`, () => {
+    const divContainer = element.query(By.css('.app-container'));
+    expect(divContainer).toBeTruthy();
+  });
+
+  it(`it should have header and content div inside the container`, () => {
+    const appHeaderEl = element.query(By.css('.app-container > .header'));
+    const routerOutletEl = element.query(By.css('.app-container > .content'));
+    expect(appHeaderEl).toBeTruthy();
+    expect(routerOutletEl).toBeTruthy();
+  });
+
+  it('should include app-header in the DOM', () => {
+    const appHeaderEl = element.query(By.css('app-header'));
+    expect(appHeaderEl).toBeTruthy();
+  });
+
+  it('should have router-outlet in the DOM', () => {
+    const routerOutletEl = element.query(By.css('router-outlet'));
+    expect(routerOutletEl).toBeTruthy();
   });
 });
